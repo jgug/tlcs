c631e46f1926c88070c3b9e52896b8808232aa9d
Pavel Vashkel <jgugn1@gmail.com>
2014-11-04 17:42:44 +0300

    Changes in TrafficLight class. Now it's takes factory object as parameter

--- src/by/bsuir/iba/Main.java
+++ src/by/bsuir/iba/Main.java
@@ -1,19 +1,21 @@
 package by.bsuir.iba;
 
-import by.bsuir.iba.enumerations.TrafficLightType;
 import by.bsuir.iba.trafficlights.TrafficLight;
+import by.bsuir.iba.trafficlights.abstractFactoryTrafficLight.FactoryBicycleTL;
+import by.bsuir.iba.trafficlights.abstractFactoryTrafficLight.FactoryPedestrianTL;
+import by.bsuir.iba.trafficlights.abstractFactoryTrafficLight.FactoryRoadTL;
 import by.bsuir.iba.trafficlights.abstractTraffiLight.AbstractTL;
 
 public class Main {
 
     public static void main(String[] args) {
-        AbstractTL trafficLight = TrafficLight.getInstance(TrafficLightType.ROAD);
+        AbstractTL trafficLight = TrafficLight.getInstance(new FactoryRoadTL());
         System.out.println(trafficLight.test());
 
-        trafficLight = TrafficLight.getInstance(TrafficLightType.PEDESTRIAN);
+        trafficLight = TrafficLight.getInstance(new FactoryPedestrianTL());
         System.out.println(trafficLight.test());
 
-        trafficLight = TrafficLight.getInstance(TrafficLightType.BICYCLE);
+        trafficLight = TrafficLight.getInstance(new FactoryBicycleTL());
         System.out.println(trafficLight.test());
     }
 }
--- src/by/bsuir/iba/trafficlights/TrafficLight.java
+++ src/by/bsuir/iba/trafficlights/TrafficLight.java
@@ -1,10 +1,6 @@
 package by.bsuir.iba.trafficlights;
 
-import by.bsuir.iba.enumerations.TrafficLightType;
 import by.bsuir.iba.trafficlights.abstractFactoryTrafficLight.AbstractFactoryTL;
-import by.bsuir.iba.trafficlights.abstractFactoryTrafficLight.FactoryBicycleTL;
-import by.bsuir.iba.trafficlights.abstractFactoryTrafficLight.FactoryPedestrianTL;
-import by.bsuir.iba.trafficlights.abstractFactoryTrafficLight.FactoryRoadTL;
 import by.bsuir.iba.trafficlights.abstractTraffiLight.AbstractTL;
 
 /**
@@ -16,32 +12,18 @@ import by.bsuir.iba.trafficlights.abstractTraffiLight.AbstractTL;
 
 public class TrafficLight {
 
+
     /**
      * Public static method for creating a new instance
-     * depending of passing to method parameter
+     * depending of passing to method traffic light factory
      *
-     * @param type is passing to method traffic light type
+     * @param factory is passing to method traffic light factory
      * @return AbstractTL
      * @see by.bsuir.iba.trafficlights.abstractTraffiLight.AbstractTL
      * @see by.bsuir.iba.enumerations.TrafficLightType
      */
-    public static AbstractTL getInstance(TrafficLightType type) {
-        AbstractFactoryTL factory;
-        switch (type) {
-            case ROAD: {
-                factory = new FactoryRoadTL();
-                return factory.createTrafficLight();
-            }
-            case PEDESTRIAN: {
-                factory = new FactoryPedestrianTL();
-                return factory.createTrafficLight();
-            }
-            case BICYCLE: {
-                factory = new FactoryBicycleTL();
-                return factory.createTrafficLight();
-            }
-        }
-        return null;
+    public static AbstractTL getInstance(AbstractFactoryTL factory) {
+        return factory.createTrafficLight();
     }
 
 }
