1c6673767504ef11063fdac894a6779ef1f940e1
Ruslan Ardytski <ardytski@gmail.com>
2014-11-10 02:50:45 +0300

    Added functionality to generate inbound transport

---
+++ src/by/bsuir/iba/gui/CrossroadCommander.java
@@ -0,0 +1,18 @@
+package by.bsuir.iba.gui;
+
+import by.bsuir.iba.core.configuration.Configuration;
+import by.bsuir.iba.core.crossroad.Crossroad;
+
+/**
+ * Created by Ruslan on 09.11.14.
+ */
+public class CrossroadCommander {
+    public void startFun(){
+        MainFrame frame = new MainFrame();
+        frame.setConfigs();
+        frame.initComponents();
+        frame.generateTransport();
+//        Crossroad crossroad = new Crossroad(Configuration.getInstance());
+//        crossroad.getCrossroadState();
+    }
+}
